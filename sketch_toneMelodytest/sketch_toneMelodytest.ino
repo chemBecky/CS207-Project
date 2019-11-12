6fa89c64bba989339a04c31b676f7e807d350ce2
chemBecky <56659384+chemBecky@users.noreply.github.com>
2019-11-12 10:07:41 -0600

    Add files via upload

---
+++ sketch_toneMelodytest/sketch_toneMelodytest.ino
@@ -0,0 +1,40 @@
+/*
+  Test for the Annie theme Easter Egg
+
+  A piezo speaker attached to pin 11 (and ground) will play the tune from Annie.
+
+  Adapted from Melody:
+  created 21 Jan 2010
+  modified 30 Aug 2011
+  by Tom Igoe
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/Tone
+*/
+
+// notes in the melody
+int melody[] = {
+  262, 220, 233, 262, 349, 440, 392, 349, 0, 440, 392, 349, 330, 294, 392, 330, 262,
+  220, 349, 392, 440, 233
+};
+
+// note durations as multiples of 1/16
+int noteLength[] = {
+  1, 3, 1, 2, 6, 2, 2, 2, 2, 3, 1, 2, 2, 1, 3, 2, 2, 1, 11, 1, 3, 8
+};
+
+const int piezoPin = 11;
+
+void setup() {
+  
+  for (int note = 0; note < 22; note++) {  // play each note of the melody
+
+    int actualLength = 1000 / 16 * noteLength[note];  // calculate the actual note duration
+    tone(piezoPin, melody[note], actualLength);
+    delay(30); //delay between notes
+    noTone(piezoPin);
+  }
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
