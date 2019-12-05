f92ac53fdeddd85ea6fb6cc07688753600d5bc86
chemBecky <56659384+chemBecky@users.noreply.github.com>
2019-12-05 12:35:30 -0600

    more variables to increase versatility
    split reachPosDelay into reachOpenDelay and reachClosedDelay, updated description, made servoPin variable

--- sketch_servotest/sketch_servotest.ino
+++ sketch_servotest/sketch_servotest.ino
@@ -1,9 +1,10 @@
 /*
- Servo testing for auto titrator
+ Servo testing for Auto Titrator
 
- This code is for testing a servo when attached to a buret.  Adjust the reachPosDelay to ensure enough time for the servo
- to rotate to the correct position.  Adjust the addTitrantDelay to add the desired volume of titrant from the buret.  The
- servo is attached to 5V, ground, and output pin 9.  The servo library is required.
+ This code is for testing a servo when attached to a buret.  Adjust the reachOpenDelay and reachClosedDelay to ensure enough
+ time for the servo to rotate to the correct position.  Adjust the addTitrantDelay to add the desired volume of titrant from
+ the buret.  Adjust the range of angles to reduce time required to move between positions.  The servo is attached to 5V,
+ ground, and output pin 9.  The servo library is required.
  
  Based on the "Sweep" example:
  by BARRAGAN <http://barraganstudio.com>
@@ -18,25 +19,30 @@
 
 Servo servo;  // create servo object to control a servo
 
-int servoPos = 0;    // variable to store the servo position
-int reachPosDelay = 25;  // delay to allow the servo to reach servoPos
+const int servoPin = 9;
+
+int servoPos;    // variable to store the servo position
+int reachOpenDelay = 25;  // delay to allow the servo to reach open position
 int addTitrantDelay = 0; // delay while titrant added 
+int reachClosedDelay = 25; // delay to allow the servo to return to closed position
 
 void setup() {
-  servo.attach(9);  // attaches the servo on pin 9 to the servo object
+  servo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-   for (; servoPos <= 80; servoPos ++) { 
+  servoPos = analogRead(servoPin);
+  
+  for (; servoPos <= 80; servoPos ++) { 
     servo.write(servoPos);              // tell servo to go to position in variable 'servoPos' in steps of 1 degree
-    delay(reachPosDelay);                       // waits for the servo to reach the position
+    delay(reachOpenDelay);              // waits for the servo to reach the open position
   }
   
   delay(addTitrantDelay);  //delay for titrant to be added
   
   for (; servoPos >= 30; servoPos --) { 
-    servo.write(servoPos);              // move servo back to starting position
-    delay(reachPosDelay);                       
+    servo.write(servoPos);              // move servo back to closed position
+    delay(reachClosedDelay);                       
   }
 
   delay(10000);  //10 second delay to allow buret reading
